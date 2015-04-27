47a39c3a27b8a557a6313fc2a2f4f5397a99a76f
Jorge J Barroso <jorge@karumi.com>
2015-04-27 14:10:55 +0200

    Add copyright to all clases, The next time this this must be on other branch.

--- rosie/src/main/java/com/karumi/rosie/domain/usercase/ClassUtils.java
+++ rosie/src/main/java/com/karumi/rosie/domain/usercase/ClassUtils.java
@@ -1,3 +1,19 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2014 karumi Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to
+  * do so, subject to the following conditions: The above copyright notice and this permission
+  * notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE
+  * IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.karumi.rosie.domain.usercase;
 
 import java.util.HashMap;
@@ -8,6 +24,7 @@ import java.util.HashMap;
 final class ClassUtils {
 
   private static HashMap<Class, Class> primitiveWrappers = new HashMap<Class, Class>();
+
   static {
     primitiveWrappers.put(Boolean.class, boolean.class);
     primitiveWrappers.put(Byte.class, byte.class);
@@ -19,7 +36,6 @@ final class ClassUtils {
     primitiveWrappers.put(Double.class, double.class);
     primitiveWrappers.put(Void.class, void.class);
   }
-
 
   public static boolean canAssign(Class<?> baseClass, Class<?> classToCheck) {
     if (baseClass.isAssignableFrom(classToCheck)) {
