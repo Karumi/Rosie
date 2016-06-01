dda8c0ecbe353627b131b561ba00d0adbb671a97
Luis Ibanez <luis.ibanez@mobile-5.com>
2016-06-01 09:25:24 +0100

    Updated support lib version Added MarvelRenderer

--- sample/src/main/java/com/karumi/rosie/sample/base/view/renderer/MarvelRenderer.java
+++ sample/src/main/java/com/karumi/rosie/sample/base/view/renderer/MarvelRenderer.java
@@ -1,7 +1,26 @@
 package com.karumi.rosie.sample.base.view.renderer;
 
+import android.view.View;
+import butterknife.ButterKnife;
+import com.pedrogomez.renderers.Renderer;
+
 /**
- * Created by libanez on 31/05/2016.
+ * Renderer extension create to provide Butter Knife view injection in a transparent way. Your
+ * Renderer classes should extend from this one to be able tu use Butter Knife annotations.
+ * Remember to call super in you overridden render method.
  */
-public class MarvelRenderer {
+
+public abstract class MarvelRenderer<T> extends Renderer<T> {
+
+    @Override public void render() {
+        ButterKnife.bind(this, getRootView());
+    }
+
+    @Override protected void setUpView(View view) {
+
+    }
+
+    @Override protected void hookListeners(View view) {
+
+    }
 }
